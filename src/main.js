da8eb73a629979d7f0ed218dc72673db03d38b39
David Dwi Nugroho <daviddwinugraha2@gmail.com>
2024-12-17 15:52:49 +0700

    belajar struktur view, route, dan components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// import './assets/main.css'
+import '../src/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeroSection from '@/components/HeroSection.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <!-- Hero Section -->
+    <HeroSection />
+  </div>
 </template>
